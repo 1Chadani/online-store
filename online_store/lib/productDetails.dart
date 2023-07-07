c6c46963508501b078dea0c2ac74d4566adb79c9
1Chadani <chad9sta@gmail.com>
2023-07-07 08:42:44 +0545

    First commit

---
+++ online_store/lib/productDetails.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _ProductDetailState extends StatefulWidget {
+  const _ProductDetailState({super.key});
+
+  @override
+  State<_ProductDetailState> createState() => __ProductDetailStateState();
+}
+
+class __ProductDetailStateState extends State<_ProductDetailState> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
